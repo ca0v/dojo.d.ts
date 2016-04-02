10d8faf3e2c57181815270c02c11374b4cc247ee
coreyalix <coreyalix@hotmail.com>
2016-04-02 15:52:32 -0400

    fixes mixin

--- dojo-tests.ts
+++ dojo-tests.ts
@@ -1,7 +1,14 @@
 /// <reference path="dojo.d.ts" />
+import lang = require("dojo/_base/lang");
 
 function test_require() {
     require(["foo"], foo => 1);
     require({ config: 1 });
-    define(["foo"], () => 1);
+    define(["foo"], () => 1);    
+}
+
+function test_mixin() {
+    let ab = lang.mixin({a:1}, {b:2});
+    ab.a;
+    ab.b;
 }
--- dojo.d.ts
+++ dojo.d.ts
@@ -5689,7 +5689,7 @@ declare namespace dojo {
              * @param dest The object to which to copy/add all properties contained in source. If dest is falsy, thena new object is manufactured before copying/adding properties begins.
              * @param sources One of more objects from which to draw all properties to copy into dest. sources are processedleft-to-right and if more than one of these objects contain the same property name, the right-mostvalue "wins".
              */
-            mixin(dest: Object, sources: Object[]): Object;
+            mixin<P,Q>(dest: P, ...sources: Q[]): P & Q;
             /**
              * similar to hitch() except that the scope object is left to be
              * whatever the execution context eventually becomes.
